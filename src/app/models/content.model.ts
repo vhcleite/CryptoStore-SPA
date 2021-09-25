70e0246e292b1bf4ff973d8a53f22dff7d2b6fac
Victor Leite <victor_leite@outlook.com.br>
2021-09-25 17:44:05 -0300

    melhorando interface de detalhes de um conteudo e consumindo mensagem de compra pendente do backend.

--- src/app/models/content.model.ts
+++ src/app/models/content.model.ts
@@ -3,22 +3,22 @@ export class Content {
     public name: string;
     public description: string;
     public price: number;
-    public contentStatus: string;
-    public status_pagamento: string;
+    public status: string;
+    public payment_status: string;
 
     constructor(
         id: number,
         name: string,
         description: string,
         price: number,
-        contentStatus: string,
-        status_pagamento: string
+        status: string,
+        payment_status: string
     ) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
-        this.contentStatus = contentStatus;
-        this.status_pagamento = status_pagamento;
+        this.status = status;
+        this.payment_status = payment_status;
     }
 }
